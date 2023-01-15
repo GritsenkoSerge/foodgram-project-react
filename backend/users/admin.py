4000ea5d2111fd556f1ece61897b3b4d12c33133
Gritsenko Serge <gritsenko.serge.2013@yandex.ru>
2023-01-16 01:09:58 +0600

    change UserAdmin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,51 +1,9 @@
-from django import forms
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.contrib.auth.models import Group
-from django.core.exceptions import ValidationError
 
 from .models import Subscription, User
-
-
-class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label="Подтверждение пароля", widget=forms.PasswordInput
-    )
-
-    class Meta:
-        model = User
-        fields = ("email",)
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Пароли не совпадают.")
-        return password2
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
-
-class UserChangeForm(forms.ModelForm):
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = User
-        fields = (
-            "email",
-            "password",
-            "is_active",
-            "is_superuser",
-            "is_staff",
-        )
 
 
 @admin.register(User)
@@ -59,9 +17,6 @@ class UserAdmin(BaseUserAdmin):
     def recipe_amount(self):
         """Количество рецептов для вывода в админке."""
         return self.recipes.count()
-
-    form = UserChangeForm
-    add_form = UserCreationForm
 
     list_display = (
         "email",
@@ -88,9 +43,10 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
         (
-            "Доступ",
+            "Права доступа",
             {
                 "fields": (
+                    "is_active",
                     "is_superuser",
                     "is_staff",
                 )
