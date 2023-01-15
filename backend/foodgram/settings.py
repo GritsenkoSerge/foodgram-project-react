91e1426ac6ebe0e23d2c1ede90d19c35f95c8d49
Gritsenko Serge <gritsenko.serge.2013@yandex.ru>
2023-01-15 20:56:20 +0600

    fix permissions for /api/users/{id}/

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -128,6 +128,7 @@ DJOSER = {
         "current_user": "api.serializers.UserSerializer",
     },
     "PERMISSIONS": {
+        "user": ["rest_framework.permissions.IsAuthenticated"],
         "user_list": ["rest_framework.permissions.AllowAny"],
     },
 }
