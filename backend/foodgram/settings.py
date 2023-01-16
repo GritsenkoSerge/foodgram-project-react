f9fc0bc6bc84db957a967185a810aea9bebf6301
Gritsenko Serge <gritsenko.serge.2013@yandex.ru>
2023-01-16 13:13:06 +0600

    make page-limit pagination

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "api.pagination.PageNumberLimitPagination",
     "PAGE_SIZE": 6,
     "NON_FIELD_ERRORS_KEY": "errors",
 }
